f06ab77769714edd97f926b144412e06e88293dd
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-07-15 18:07:50 +0300

    index.js updated

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import './assets/global.css'
+import { AuthenticationProvider } from './context/Authentication'
+import { ThemeProvider } from './context/Theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <AuthenticationProvider>
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
+    </AuthenticationProvider>
 )
 
 reportWebVitals()
