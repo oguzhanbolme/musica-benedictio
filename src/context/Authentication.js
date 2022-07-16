152475214bdf480513ef99555c40c80ac0d400f5
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-07-17 01:52:42 +0300

    authentication context updated

--- src/context/Authentication.js
+++ src/context/Authentication.js
@@ -1,10 +1,30 @@
-import React, { createContext } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 
 export const AuthenticationContext = createContext()
 
 export function AuthenticationProvider({ children }) {
+    const [accessToken, setAccessToken] = useState(null)
+
+    useEffect(() => {
+        const hash = window.location.hash
+        let token = window.localStorage.getItem('access_token')
+
+        if (!token && hash) {
+            token = hash
+                .substring(1)
+                .split('&')
+                .find((elem) => elem.startsWith('access_token'))
+                .split('=')[1]
+
+            window.location.hash = ''
+            window.localStorage.setItem('access_token', token)
+        }
+
+        setAccessToken(token)
+    }, [])
+
     return (
-        <AuthenticationContext.Provider value={0}>
+        <AuthenticationContext.Provider value={{ accessToken, setAccessToken }}>
             {children}
         </AuthenticationContext.Provider>
     )
