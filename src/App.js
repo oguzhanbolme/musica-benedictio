b3bf8f7d7e3ffd1b733d5d4289ab3dd7e4f43fb7
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-07-17 01:54:56 +0300

    app.js updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { AuthenticationContext } from './context/Authentication'
+import Navbar from './layouts/Navbar'
+import PageContainer from './layouts/PageContainer'
+import Artist from './pages/Artist'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
+import Track from './pages/Track'
 
 export default function App() {
-    return <div>App</div>
+    const { accessToken } = useContext(AuthenticationContext)
+
+    return (
+        <div className="h-screen bg-green-300">
+            <Navbar />
+            <PageContainer>
+                <BrowserRouter>
+                    <Routes>
+                        <Route exact path="/" element={!accessToken ? <Login /> : <Navigate to="/profile" />} />
+                        {accessToken && (
+                            <>
+                                <Route path="/profile" element={<Profile />} />
+                                <Route path="/discover-artist" element={<Artist />} />
+                                <Route path="/track-analysis" element={<Track />} />
+                            </>
+                        )}
+                        <Route path="*" element={!accessToken ? <Login /> : <Navigate to="/profile" />} />
+                    </Routes>
+                </BrowserRouter>
+            </PageContainer>
+        </div>
+    )
 }
