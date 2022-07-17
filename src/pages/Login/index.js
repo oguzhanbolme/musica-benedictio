b3273d271a5d00fa7239b6e0ceda8e44b62733de
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-07-17 17:12:57 +0300

    login page updated

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -5,7 +5,7 @@ export default function Login() {
     return (
         <div className="h-screen flex items-center justify-center">
             <a
-                href={`${process.env.REACT_APP_AUTH_ENDPOINT}?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=${process.env.REACT_APP_RESPONSE_TYPE}`}
+                href={`${process.env.REACT_APP_AUTH_ENDPOINT}?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=${process.env.REACT_APP_RESPONSE_TYPE}&scope=user-top-read user-read-private`}
             >
                 <Button title="Login to Spotify" />
             </a>
