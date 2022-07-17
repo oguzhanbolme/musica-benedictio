1b884abf2fa62b42a86b869d7e9bb02c09473da2
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-07-17 17:05:00 +0300

    authentication context updated

--- src/context/Authentication.js
+++ src/context/Authentication.js
@@ -1,36 +1,30 @@
-import React, { createContext, useEffect, useState } from 'react'
+import React, { createContext, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import useLocalStorage from '../hooks/useLocalStorage'
 
 export const AuthenticationContext = createContext()
 
 export function AuthenticationProvider({ children }) {
     const navigate = useNavigate()
-    const [accessToken, setAccessToken] = useState(null)
-    const updateAccessToken = (val) => {
-        if (!val) window.localStorage.removeItem('access_token')
-        setAccessToken(val)
-    }
+    const [accessToken, setAccessToken] = useLocalStorage('access_token', null)
 
     useEffect(() => {
         const hash = window.location.hash
-        let token = window.localStorage.getItem('access_token')
 
-        if (!token && hash) {
-            token = hash
+        if (!accessToken && hash) {
+            const token = hash
                 .substring(1)
                 .split('&')
                 .find((elem) => elem.startsWith('access_token'))
                 .split('=')[1]
 
-            window.localStorage.setItem('access_token', token)
+            setAccessToken(token)
             navigate('/profile')
         }
-
-        updateAccessToken(token)
     }, [])
 
     return (
-        <AuthenticationContext.Provider value={{ accessToken, updateAccessToken }}>
+        <AuthenticationContext.Provider value={{ accessToken, setAccessToken }}>
             {children}
         </AuthenticationContext.Provider>
     )
