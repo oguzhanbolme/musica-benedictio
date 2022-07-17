b5fbcd70db67af95505056499ef4bc5a5b38fda4
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-07-17 16:03:19 +0300

    login page updated

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import Button from '../../components/ui/Button'
 
 export default function Login() {
-    return <div>Login</div>
+    return (
+        <div className="h-screen flex items-center justify-center">
+            <a
+                href={`${process.env.REACT_APP_AUTH_ENDPOINT}?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=${process.env.REACT_APP_RESPONSE_TYPE}`}
+            >
+                <Button title="Login to Spotify" />
+            </a>
+        </div>
+    )
 }
