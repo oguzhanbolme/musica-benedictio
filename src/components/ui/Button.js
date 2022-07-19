a927923aae0ec9809706fd38597657224a707a09
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-07-20 01:23:16 +0300

    button created

---
+++ src/components/ui/Button.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Button({ title, onClick }) {
+    return (
+        <button
+            className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-lg font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+            onClick={onClick}
+        >
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                {title}
+            </span>
+        </button>
+    )
+}
