47984806e74ff5678c664cc3d63cd1da4549eea9
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-07-20 01:24:21 +0300

    useSpotify custom hook created

---
+++ src/hooks/useSpotify.js
@@ -0,0 +1,45 @@
+import { useContext, useEffect, useState } from 'react'
+import axios from 'axios'
+import { AuthenticationContext } from '../context/Authentication'
+
+const base = 'https://api.spotify.com'
+
+export default function useSpotify(endpoint) {
+    const { accessToken, setAccessToken } = useContext(AuthenticationContext)
+    const [loading, setLoading] = useState(false)
+    const [data, setData] = useState(null)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        setLoading(true)
+        setData(null)
+        setError(null)
+
+        axios({
+            url: base + endpoint,
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        })
+            .then((response) => {
+                setLoading(false)
+                setData(response.data)
+            })
+            .catch((error) => {
+                console.log(error)
+                if (error.response.status === 401) {
+                    setAccessToken(null)
+                } else {
+                    console.log(error)
+                    setLoading(false)
+                    setError({
+                        code: error.response.status,
+                        message: error.message,
+                    })
+                }
+            })
+    }, [])
+
+    return { data, loading, error }
+}
