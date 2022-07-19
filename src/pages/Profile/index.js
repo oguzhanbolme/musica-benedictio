3432985003d4a4976f92da35ee95c51394bba021
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-07-20 01:24:42 +0300

    profile page updated

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,35 @@
 import React from 'react'
+import useSpotify from '../../hooks/useSpotify'
+import Suspense from '../../components/Suspense'
+import Me from './Me'
+import TopArtists from './TopArtists'
+import TopTracks from './TopTracks'
 
 export default function Profile() {
-    return <div>Profile</div>
+    const { data: profile, loading: profileLoading, error: profileError } = useSpotify('/v1/me')
+    const { data: artists, loading: artistsLoading, error: artistsError } = useSpotify('/v1/me/top/artists?limit=10')
+    const { data: tracks, loading: tracksLoading, error: tracksError } = useSpotify('/v1/me/top/tracks?limit=10')
+
+
+    return (
+        <div className="flex md:flex-row flex-col items-center justify-evenly mt-0 md:mt-10 pb-10 md:pb-0 gap-10 md:gap-0">
+            <div className="flex flex-col items-center order-1 md:order-2">
+                <Suspense isLoading={profileLoading} error={profileError}>
+                    <Me profile={profile} />
+                </Suspense>
+            </div>
+
+            <div className="order-3">
+                <Suspense isLoading={artistsLoading} error={artistsError}>
+                    <TopArtists artists={artists} />
+                </Suspense>
+            </div>
+
+            <div className="order-2 md:order-1">
+                <Suspense isLoading={tracksLoading} error={tracksError}>
+                    <TopTracks tracks={tracks} />
+                </Suspense>
+            </div>
+        </div>
+    )
 }
