8fa82e06127fcdf2bc7401ebe2fabdf076b1c5b1
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-07-24 12:31:41 +0300

    debounce util created

---
+++ src/utils/debounce.js
@@ -0,0 +1,8 @@
+export default function debounce(func, timeout) {
+    let timer
+
+    return (...args) => {
+        clearTimeout(timer)
+        timer = setTimeout(() => func.apply(this, args), timeout)
+    }
+}
