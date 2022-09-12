0faa303b64db983c69b3b7312dc54e9d52d79a7b
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-09-12 20:12:52 +0300

    me component updated

--- src/pages/Profile/Me.js
+++ src/pages/Profile/Me.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import Icon from '@mdi/react'
+import { mdiAccountCircle } from '@mdi/js'
 
 export default function Me({ profile }) {
     return (
         <>
-            <img
-                className="w-36 h-36 md:w-36 md:h-36 lg:w-48 lg:h-48 rounded-full"
-                src={profile?.images[0].url}
-                alt="Avatar"
-            />
+            {profile?.images && profile?.images[0]?.url ? (
+                <img
+                    className="w-36 h-36 md:w-36 md:h-36 lg:w-48 lg:h-48 rounded-full"
+                    src={profile?.images[0]?.url}
+                    alt="Avatar"
+                />
+            ) : (
+                <Icon
+                    className="w-36 h-36 md:w-36 md:h-36 lg:w-48 lg:h-48 rounded-full"
+                    path={mdiAccountCircle}
+                />
+            )}
             <span className="text-lg font-bold">{profile?.display_name}</span>
             <span>{profile?.followers.total} Followers</span>
         </>
